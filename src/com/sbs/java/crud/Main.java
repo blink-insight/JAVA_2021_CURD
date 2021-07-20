739e193158a4ad5ef17b0c8280391b5fe1abddf7
blink-insight <dksgorud99@naver.com>
2021-07-20 11:44:04 +0900

    게시글 상세내용입력

--- src/com/sbs/java/crud/Main.java
+++ src/com/sbs/java/crud/Main.java
@@ -2,27 +2,28 @@ package com.sbs.java.crud;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("==프로그램 시작==");
+		System.out.println("== 프로그램 시작 ==");
 
 		Scanner sc = new Scanner(System.in);
 
 		int lastArticleId = 0;
 
-		List<Article> articles = new ArrayList<Article>();
+		List<Article> articles = new ArrayList<>();
 
 		while (true) {
-			System.out.printf("명령어)");
-			String command = sc.nextLine().trim();
-			System.out.printf("입력된 명령어 : %s\n", command);
+			System.out.printf("명령어) ");
+			String command = sc.nextLine();
+
+			command = command.trim();
 
 			if (command.length() == 0) {
 				continue;
 			}
+
 			if (command.equals("system exit")) {
 				break;
 			}
@@ -37,32 +38,66 @@ public class Main {
 				Article article = new Article(id, title, body);
 				articles.add(article);
 
-				
-
-				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+				System.out.printf("%d번글이 생성되었습니다.\n", id);
 
 			} else if (command.equals("article list")) {
+
 				if (articles.size() == 0) {
-					System.out.println("게시물이 없습니다.");
-				} else 
-					System.out.printf("번호   |   제목\n");
-					
-					for (int i = articles.size() - 1; i >= 0; i--) {
-						Article article = articles.get(i);
-						
-						System.out.printf("%d    |    %s\n", article.id, article.title);
+					System.out.println("게시글이 없습니다.");
+					continue;
+				}
+				System.out.print("번호 | 제목\n");
+
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+
+					System.out.printf("%d   |    %s\n", article.id, article.title);
+				}
+			} else if (command.startsWith("article detail ")) {
+				String[] commandBits = command.split(" ");
+
+				int id = Integer.parseInt(commandBits[2]);
+
+				
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+						break;
 					}
 				}
-				else {
-					System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.", command);
+				
+				
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
 				}
+
+				else {
+					System.out.printf("번호 : %d\n", foundArticle.id);
+					System.out.printf("날짜 : 2021-12-12 12:12:12\n");
+					System.out.printf("제목 : %d\n", foundArticle.title);
+					System.out.printf("내용 : %d\n", foundArticle.body);
+					continue;
+				}
+
 			}
 
-			sc.close();
-			System.out.println("==프로그램  끝==");
+			else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
+			}
 
 		}
+
+		sc.close();
+
+		System.out.println("== 프로그램 끝 ==");
 	}
+}
 
 class Article {
 	int id;
@@ -73,7 +108,5 @@ class Article {
 		this.id = id;
 		this.title = title;
 		this.body = body;
-
 	}
-
 }
